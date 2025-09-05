b2a6df699bac5b87c5039d41f4b10a6c47ddbaec
guilherme canechia <canechiagui@gmail.com>
2025-09-05 15:23:00 -0500

    Fix map markers not loading by configuring Leaflet default icons

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -6,6 +6,15 @@ import { useEffect } from 'react';
 import CityCard from './CityCard';
 import { useSelector } from 'react-redux';
 import NearMeIcon from '@mui/icons-material/NearMe';
+import L from 'leaflet';
+
+// Fix for default markers in react-leaflet
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+});
 
 interface MapProps {
   locations: Location[];
